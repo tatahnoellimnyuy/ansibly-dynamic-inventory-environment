5158cf62c223c643944fefc37ce31d3fa427b500
tatahnoellimnyuy <tatahnoellimnyuy@gmail.com>
2023-05-05 17:32:33 +0200

    added terraform code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dynamic-inventory
+
+- this is the terraform code to set up a text environment with 3 servers, 
+- one of the server is the ansible master and the other 2 are nodes with environment tags dev and qa.
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#install amazon ansible-ec2 plugin
+
+sudo ansible-galaxy collection install amazon.aws
+# install ansible with python3
+
+sudo pip3 install ansible
+
+#install boto3 and botocore
+sudo pip3 install boto3
